2cc24b66aede8f5eb67859fca02c6c8de495057d
fox.cpp <fox.cpp@disroot.org>
2019-05-31 17:45:15 +0300

    autodiscovery: Drop miekg/dns

--- autodiscovery/dns.go
+++ autodiscovery/dns.go
@@ -4,9 +4,8 @@ package autodiscovery
 
 import (
 	"errors"
+	"net"
 	"strconv"
-
-	"github.com/miekg/dns"
 )
 
 var ErrNoService = errors.New("autodiscovery: no DNS SRV record")
@@ -14,24 +13,14 @@ var ErrNoService = errors.New("autodiscovery: no DNS SRV record")
 // Probe attempts to perform JMAP service autodiscovery by looking for DNS SRV
 // entry. It returns URL of JMAP session resource. ErrNoService is returned in
 // case there is no DNS SRV record for passed domain.
-//
-// Since Go standard DNS operations don't support SRV queries, custom DNS
-// library is used and you have to provide related configuration options.
-func Probe(cl *dns.Client, cfg *dns.ClientConfig, domain string) (string, error) {
-	m := new(dns.Msg)
-	m.SetQuestion("_jmap._tcp."+dns.Fqdn(domain), dns.TypeSRV)
-	resp, _, err := cl.Exchange(m, cfg.Servers[0]+":"+cfg.Port)
+func Probe(domain string) (string, error) {
+	_, addrs, err := net.LookupSRV("jmap", "tcp", domain)
 	if err != nil {
 		return "", err
 	}
 
-	for _, ans := range resp.Answer {
-		srv, ok := ans.(*dns.SRV)
-		if !ok {
-			continue
-		}
-
-		return "https://" + srv.Target + ":" + strconv.Itoa(int(srv.Port)) + "/.well-known/jmap", nil
+	for _, addr := range addrs {
+		return "https://" + addr.Target + ":" + strconv.Itoa(int(addr.Port)) + "/.well-known/jmap", nil
 	}
 	return "", ErrNoService
 }
--- go.mod
+++ go.mod
@@ -2,11 +2,6 @@ module github.com/foxcpp/go-jmap
 
 require (
 	github.com/google/go-cmp v0.3.0 // indirect
-	github.com/miekg/dns v1.1.9
 	github.com/pkg/errors v0.8.1 // indirect
-	golang.org/x/crypto v0.0.0-20190513172903-22d7a77e9e5f // indirect
-	golang.org/x/net v0.0.0-20190514140710-3ec191127204 // indirect
-	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
-	golang.org/x/sys v0.0.0-20190515120540-06a5c4944438 // indirect
 	gotest.tools v2.2.0+incompatible
 )
